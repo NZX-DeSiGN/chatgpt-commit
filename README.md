74dfa3b574bab7fa1219e8d53d0e52b2e3b5c8e0
Nicolas Brobst <nicobrobst@gmail.com>
2023-03-10 00:43:49 +0100

    feat(vscode extension): Add VSCode extension framework
    - Add necessary VSCode extension files and package dependencies.

---
+++ README.md
@@ -0,0 +1,5 @@
+# chatgpt-commit
+
+### 1.0.0
+
+Initial release of ChatGPT commit
---
+++ extension.js
@@ -0,0 +1,14 @@
+const vscode = require("vscode")
+const extension = "chatgpt-commit"
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate,
+}
